32e8b9e01de6c0d44a15d034014b0ef9c83db3ec
mohit chopra <mohitchopra5743@gmail.com>
2024-03-24 12:41:34 +0530

    init

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,5 @@
 import supabase from "./supabase";
+import { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -11,15 +12,23 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.imagename}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/sign/cabin-images/$`;
+
+  //https://huoqiyvsksclfnetgiep.supabase.co/storage/v1/object/sign/cabin-images/cabin-002.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWJpbi1pbWFnZXMvY2FiaW4tMDAyLmpwZyIsImlhdCI6MTcxMTI2MDg3OCwiZXhwIjoxNzQyNzk2ODc4fQ.pj8ZCM9-b_HTTlNC03CKoR8AohYG86mPxri3vItGtdQ&t=2024-03-24T06%3A14%3A33.766Z
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
-    .select();
+    .insert([{ ...newCabin, image: imagePath }]);
 
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created");
   }
+
   return data;
 }
 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://huoqiyvsksclfnetgiep.supabase.co";
+export const supabaseUrl = "https://huoqiyvsksclfnetgiep.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1b3FpeXZza3NjbGZuZXRnaWVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwMTczNTMsImV4cCI6MjAyNjU5MzM1M30.sqr2De_XIBiYPyRuOoCVoxPVupeHCPnBomDZtQ_Ahg0";
 const supabase = createClient(supabaseUrl, supabaseKey);
