57ff407c348414bd4d097e6c2061f47cc591b5df
mohit chopra <mohitchopra5743@gmail.com>
2024-04-01 01:21:39 +0530

    m

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,24 +2,24 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
-import SpinnerMini from "../../ui/SpinnerMini";
 import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
-  const [email, setEmail] = useState("temp@example.com");
-  const [password, setPassword] = useState("test1234");
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!email || !password) return;
-
     login(
       { email, password },
       {
         onSettled: () => {
-          setEmail(""), setPassword("");
+          setEmail("");
+          setPassword("");
         },
       }
     );
@@ -33,19 +33,20 @@ function LoginForm() {
           id="email"
           // This makes this form better for password managers
           autoComplete="username"
-          disabled={isLoading}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
+
       <FormRowVertical label="Password">
         <Input
           type="password"
           id="password"
           autoComplete="current-password"
-          disabled={isLoading}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
